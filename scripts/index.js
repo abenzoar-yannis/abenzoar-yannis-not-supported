c72f7fcd6f814f276f3ec5c00e82cad73571c3dc
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-01-13 02:04:22 +0100

    ajout des commandes Javascript pour ouvrir et fermer mes menus

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,17 @@ const menuLeft = document.querySelector(".navBar__menuLeft > ul");
 const menuCenter = document.querySelector(".navBar__menuCenter > ul");
 const menuRight = document.querySelector(".navBar__menuRight > ul");
 
+// Commande pour faire apparaître et disparaître mes menus
 buttonSlideLeft.addEventListener("click", () => {
   menuLeft.classList.toggle("translateRight");
+});
+
+buttonSlideRight.addEventListener("click", () => {
+  menuRight.classList.toggle("translateLeft");
+  buttonSlideRight.classList.toggle("reverse");
+});
+
+buttonSlideCenter.addEventListener("click", () => {
+  menuCenter.classList.toggle("translateDown");
+  buttonSlideCenter.classList.toggle("reverse");
 });
