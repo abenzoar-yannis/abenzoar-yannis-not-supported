325c5dbf096a7c8062155ef15e61a12b6da0636c
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-01-13 01:40:31 +0100

    Déclaration des boutons et des boites menu, dans mon fichier JS

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+// Déclaration des bouton et des boite pour mes menu damovible
+const buttonSlideLeft = document.querySelector(".buttonSlideLeft");
+const buttonSlideCenter = document.querySelector(".buttonSlideCenter");
+const buttonSlideRight = document.querySelector(".buttonSlideRight");
+const menuLeft = document.querySelector(".navBar__menuLeft > ul");
+const menuCenter = document.querySelector(".navBar__menuCenter > ul");
+const menuRight = document.querySelector(".navBar__menuRight > ul");
